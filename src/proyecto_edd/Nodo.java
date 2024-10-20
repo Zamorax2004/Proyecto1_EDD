8277e22b6a7898eb9aaadf210d625cd7df41c768
Ignacio <ignacio.zamora@correo.unimet.edu.ve>
2024-10-19 22:47:36 -0400

    Clase Nodo

--- src/proyecto_edd/Nodo.java
+++ src/proyecto_edd/Nodo.java
@@ -1,8 +1,27 @@
 package proyecto_edd;
 
 public class Nodo {
+    private Object data;
+    private Nodo next;
+    
+    public Nodo(Object data){
+        this.data = data;
+        this.next = null;
+    }
 
-    public static void main(String[] args) {
+    public Object getData() {
+        return data;
+    }
 
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Nodo getNext() {
+        return next;
+    }
+
+    public void setNext(Nodo next) {
+        this.next = next;
     }
 }
